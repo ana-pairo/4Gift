b8cd4274d1f10acb8c6f8677ec6e3540b0866da7
anapairo <ana.pairo2@gmail.com>
2023-02-01 20:02:14 -0300

    feat: change context

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -7,14 +7,17 @@ import PrivatePage from "./PrivateRoutes";
 import Home from "../pages/Home/HomePage";
 import Account from "../pages/Account/AccountPage";
 import FriendsList from "../pages/FriendsList/FriendsListPage";
+import AuthProvider from "../contexts/AuthContext";
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-in" element={<SignIn />} />
+        <AuthProvider>
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </AuthProvider>
         <Route path="/" element={<PrivatePage />}>
           <Route path="account" element={<Account />} />
           <Route path="home" element={<Home />} />
